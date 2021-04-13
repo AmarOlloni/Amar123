0eaada67e79b4f45a489ba17331e137168b30bfc
AmarOlloni <82441849+AmarOlloni@users.noreply.github.com>
2021-04-13 12:31:31 +0200

    Add files via upload

---
+++ JavaScript/Scripta.js
@@ -0,0 +1,10 @@
+var numri=0;
+
+while(numri<11){
+	
+	console.log("Numri eshte: "+ numri);
+	numri++;
+	
+}
+
+
---
+++ JavaScript/aa.js
@@ -0,0 +1,14 @@
+var numri=0;
+
+while(numri<11){
+	
+	console.log("Numri eshte: "+ numri);
+	numri++;
+
+}
+
+for(var nr=0; nr<11; nr++){
+   console.log("Numri eshte:"+ nr);
+}
+
+
